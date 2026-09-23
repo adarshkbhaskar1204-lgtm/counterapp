e3d1d4396f63756f9671a0016311c9e8717c3cd2
demo1 <adarshkbhaskar1204@gmail.com>
2026-09-23 16:11:30 +0530

    Added React project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Cp from './Cp'
+import './counter.css'
+function App() {
+  return (
+    <div className="App">
+      <h1>Counter Program </h1>
+      <Cp/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Cp.js
@@ -0,0 +1,25 @@
+import React from "react";
+import './counter.css'
+class Cp extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            number:0
+        }
+    }
+    add=()=>{
+        this.setState({number:this.state.number+1})
+
+    }
+    subtract=()=>{
+        this.setState({number:this.state.number-1})
+    }
+    render(){
+        return <div>
+            <h1>{this.state.number}</h1>
+            <button onClick={this.add}>ADD +1</button>
+            <button onClick={this.subtract}>SUBTRACT -1</button>
+        </div>
+    }
+}
+export default Cp
